a6107ceafafe5c7a4933905b1b4fc83f517f7287
LENOVO <LENOVO@LAPTOP-K1I8EVQ0>
2022-01-04 14:25:36 +0530

    newcommit

---
+++ src/Gitdemo1project/github2.java
@@ -0,0 +1,15 @@
+package Gitdemo1project;
+
+public class github2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String cricket="yes";
+if(cricket=="yes") {
+	System.out.println("i like to play cricket");
+}else {
+	
+}System.out.println("i play only at evening times");
+	}
+
+}
